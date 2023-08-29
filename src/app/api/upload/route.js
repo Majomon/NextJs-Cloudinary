4a706a19be237acd5f3e7fbea59ddc3fb3855b19
Mauri <mauri.monzon91@gmail.com>
2023-08-29 12:20:23 -0300

    Instalando cloudinary

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,14 +1,23 @@
 import { NextResponse } from "next/server";
+//Guardar archivos, leer, eliminar, etc
+import { writeFile } from "fs/promises";
+//Modulo que nos da toda la ruta de manera automatica
+import path from "path";
 
 export async function POST(request) {
   const data = await request.formData();
-  const image = !data.get("img");
-  if (image) {
+  const image = await data.get("img");
+  if (!image) {
     return NextResponse.json("No se subio la iamgen", { status: 400 });
   }
 
   //Convertir la imagen a Bytes
-  const bytes = image.arrayBuffer();
+  const bytes = await image.arrayBuffer();
   const buffer = Buffer.from(bytes);
+
+  //Ruta del archivo
+  const filePatch = path.join(process.cwd(), "public", image.name);
+  //Crear archivo
+  await writeFile(filePatch, buffer);
   return NextResponse.json("Imagen subida");
 }
