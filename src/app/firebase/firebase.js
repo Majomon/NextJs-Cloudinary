3c703ccc0f5fde4980fe5b26916adb1f7d687afd
Mauri <mauri.monzon91@gmail.com>
2023-09-01 11:09:39 -0300

    Firebase

---
+++ src/app/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB03B2Fqwj95Id_3hLQYFHFC98x947lwrY",
+  authDomain: "next-demo-firebase-hosting.firebaseapp.com",
+  projectId: "next-demo-firebase-hosting",
+  storageBucket: "next-demo-firebase-hosting.appspot.com",
+  messagingSenderId: "314316799438",
+  appId: "1:314316799438:web:df7c1c84d3012d1bffbf9f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
